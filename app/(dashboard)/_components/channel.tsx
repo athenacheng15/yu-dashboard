8141c87bb1bb574ff231fba0903c3d91594553e6
Athena Cheng <athenachengyc15@gmail.com>
2024-02-14 16:21:23 +0800

    feat: homepage text and navigation

--- app/(dashboard)/_components/channel.tsx
+++ app/(dashboard)/_components/channel.tsx
@@ -1,14 +1,19 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
 import { Code2, Home } from 'lucide-react';
 
+import { Button } from '@/components/ui/button';
+
 export const ChannelBtn = () => {
+    const router = useRouter();
+
     const channelInfo = [
-        { title: 'Home', icon: <Home className="w-4" /> },
+        { title: 'Home', icon: <Home className="w-4" />, href: '/' },
         {
             title: 'SoftWare Engineer',
             icon: <Code2 className="w-4" />,
+            href: '/software-engineer',
         },
     ];
     return (
@@ -18,6 +23,9 @@ export const ChannelBtn = () => {
                     key={item.title}
                     className="group flex w-full items-center justify-start space-x-2 text-lg"
                     variant="main"
+                    onClick={() => {
+                        router.push(item.href);
+                    }}
                 >
                     <div className="group-hover:text-main-dark-green">
                         {item.icon}
