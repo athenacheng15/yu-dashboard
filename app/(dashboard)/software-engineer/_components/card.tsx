429d40d0e0a8bf757c218abcb14b58c2df0d47af
Athena Cheng <athenachengyc15@gmail.com>
2024-03-23 15:30:18 +0800

    fix : background layout

--- app/(dashboard)/software-engineer/_components/card.tsx
+++ app/(dashboard)/software-engineer/_components/card.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { DisplayRepoType } from '@/type/repositories';
+import { DisplayRepoType } from '@/types/repositories';
 import { useRouter } from 'next/navigation';
 
 import { Separator } from '@/components/ui/separator';
@@ -11,31 +11,29 @@ export const Card = ({ repo }: { repo: DisplayRepoType }) => {
 
     return (
         <div
-            className="group col-span-1 cursor-pointer rounded-xl border-2 border-main-green bg-white/70 p-3 hover:bg-[#F2F7F2]/70 sm:p-4"
+            className="group flex cursor-pointer flex-col justify-between rounded-xl border-2 border-main-green bg-white/70 p-3 hover:bg-[#F2F7F2]/70 sm:p-4"
             onClick={() => router.push(`/software-engineer/${repo.name}`)}
         >
-            <div>
-                <div className="flex w-full flex-col gap-2">
-                    <div className="flex justify-between space-x-3">
-                        <div className="relative aspect-video w-3/4 overflow-hidden rounded-xl sm:w-full">
-                            {/* TODO image*/}
-                            <div className="h-full w-full bg-neutral-300"></div>
-                        </div>
-                        <div className="flex grow sm:hidden">
-                            <CardFooter repo={repo} />
-                        </div>
+            <div className="flex w-full flex-col gap-2">
+                <div className="flex grow justify-between space-x-3">
+                    <div className="relative aspect-video w-3/4 overflow-hidden rounded-xl sm:w-full">
+                        {/* TODO image*/}
+                        <div className="h-full w-full bg-neutral-300"></div>
                     </div>
-                    <p className="text-xl font-medium text-main-green">
-                        {repo.name}
-                    </p>
-                    <p className="text-sm font-medium text-neutral-500">
-                        {repo.description}
-                    </p>
+                    <div className="flex grow sm:hidden">
+                        <CardFooter repo={repo} />
+                    </div>
                 </div>
-                <div className="hidden sm:flex sm:flex-col sm:gap-2">
-                    <Separator className="my-2" />
-                    <CardFooter repo={repo} />
-                </div>
+                <p className="text-xl font-medium text-main-green">
+                    {repo.name}
+                </p>
+                <p className="text-sm font-medium text-neutral-500">
+                    {repo.description}
+                </p>
+            </div>
+            <div className="hidden sm:flex sm:flex-col sm:gap-2">
+                <Separator className="my-2" />
+                <CardFooter repo={repo} />
             </div>
         </div>
     );
