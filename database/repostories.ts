68b0a0c8996d45607ad28626ae3b24fe24f6970f
yuchuncheng <yuchuncheng@nine-yi.com>
2024-04-12 15:32:53 +0800

    feat : restructure database

--- database/repostories.ts
+++ database/repostories.ts
@@ -2,393 +2,96 @@ export const repostories = {
     'airbnb-clone': {
         tags: ['fullStack'],
         coverImageUrl: './images/airbnb-clone/img-1.png',
-        keyFeatures: [
-            'desc-1',
-            'desc-2',
-            'desc-3',
-            'desc-4',
-            'desc-5',
-            'desc-6',
-            'desc-7',
-            'desc-8',
-            'desc-9',
+        keyFeatures: ['desc-1', 'desc-2', 'desc-3', 'desc-4', 'desc-5', 'desc-6', 'desc-7', 'desc-8', 'desc-9'],
+        snapshots: [
+            { id: 1, desc: 'desc-1', imageUrl: './images/airbnb-clone/img-1.png' },
+            { id: 2, desc: 'desc-2', imageUrl: './images/airbnb-clone/img-2.png' },
+            { id: 3, desc: 'desc-3', imageUrl: './images/airbnb-clone/img-3.png' },
+            { id: 4, desc: 'desc-4', imageUrl: './images/airbnb-clone/img-4.png' },
+            { id: 5, desc: 'desc-5', imageUrl: './images/airbnb-clone/img-5.png' },
+            { id: 6, desc: 'desc-6', imageUrl: './images/airbnb-clone/img-6.png' },
+            { id: 7, desc: 'desc-7', imageUrl: './images/airbnb-clone/img-7.png' },
+            { id: 8, desc: 'desc-8', imageUrl: './images/airbnb-clone/img-8.png' },
+            { id: 9, desc: 'desc-9', imageUrl: './images/airbnb-clone/img-9.png' },
+            { id: 10, desc: 'desc-10', imageUrl: './images/airbnb-clone/img-10.png' },
+            { id: 11, desc: 'desc-11', imageUrl: './images/airbnb-clone/img-11.png' },
+            { id: 12, desc: 'desc-12', imageUrl: './images/airbnb-clone/img-12.png' },
         ],
-        snapshot: [
-            {
-                id: 1,
-                desc: '',
-                imageUrl: './images/airbnb-clone/img-1.png',
-            },
-            {
-                id: 2,
-                desc: '',
-                imageUrl: './images/airbnb-clone/img-2.png',
-            },
-            {
-                id: 3,
-                desc: '',
-                imageUrl: './images/airbnb-clone/img-3.png',
-            },
-            {
-                id: 4,
-                desc: '',
-                imageUrl: './images/airbnb-clone/img-4.png',
-            },
-            {
-                id: 5,
-                desc: '',
-                imageUrl: './images/airbnb-clone/img-5.png',
-            },
-            {
-                id: 6,
-                desc: '',
-                imageUrl: './images/airbnb-clone/img-6.png',
-            },
-            {
-                id: 7,
-                desc: '',
-                imageUrl: './images/airbnb-clone/img-7.png',
-            },
-            {
-                id: 8,
-                desc: '',
-                imageUrl: './images/airbnb-clone/img-8.png',
-            },
-            {
-                id: 9,
-                desc: '',
-                imageUrl: './images/airbnb-clone/img-9.png',
-            },
-            {
-                id: 10,
-                desc: '',
-                imageUrl: './images/airbnb-clone/img-9.png',
-            },
-            {
-                id: 11,
-                desc: '',
-                imageUrl: './images/airbnb-clone/img-9.png',
-            },
-            {
-                id: 12,
-                desc: '',
-                imageUrl: './images/airbnb-clone/img-9.png',
-            },
-        ],
-        techStacks: [
-            'next-js',
-            'typescript',
-            'tailwindcss',
-            'prisma',
-            'mongodb',
-        ],
-        webDevToolkit: [
-            {
-                id: 'nextauth-js',
-                logoUrl:
-                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLiiZD803XYeFxyVSPRNfI8SxO4UABqJcGyreAGxUmUw&s',
-                name: 'NextAuth.js',
-                desc: 'nextauth-desc',
-            },
-            {
-                id: 'next-cloudinary',
-                logoUrl:
-                    'https://pbs.twimg.com/profile_images/1674799535665684482/SgGTyzXN_400x400.png',
-                name: 'Next Cloudinary',
-                desc: 'next-cloudinary-desc',
-            },
-            {
-                id: 'zustand',
-                logoUrl: 'https://img.stackshare.io/service/11559/zustand.png',
-                name: 'Zustand',
-                desc: 'zustand-desc',
-            },
+        techStacks: ['next-js', 'typescript', 'tailwindcss', 'prisma', 'mongodb'],
+        webDevToolkits: [
+            { id: 'nextauth-js', name: 'NextAuth.js', desc: 'nextauth-desc' },
+            { id: 'next-cloudinary', name: 'Next Cloudinary', desc: 'next-cloudinary-desc' },
+            { id: 'zustand', name: 'Zustand', desc: 'zustand-desc' },
         ],
     },
     'trello-clone': {
         tags: ['fullStack'],
         coverImageUrl: './images/trello-clone/img-1.png',
-        keyFeatures: [
-            'desc-1',
-            'desc-2',
-            'desc-3',
-            'desc-4',
-            'desc-5',
-            'desc-6',
-            'desc-7',
-            'desc-8',
-        ],
-        snapshot: [
-            {
-                id: 1,
-                desc: '',
-                imageUrl: './images/trello-clone/img-1.png',
-            },
-            {
-                id: 2,
-                desc: '',
-                imageUrl: './images/trello-clone/img-2.png',
-            },
-            {
-                id: 3,
-                desc: '',
-                imageUrl: './images/trello-clone/img-3.png',
-            },
-            {
-                id: 4,
-                desc: '',
-                imageUrl: './images/trello-clone/img-4.png',
-            },
-            {
-                id: 5,
-                desc: '',
-                imageUrl: './images/trello-clone/img-5.png',
-            },
-            {
-                id: 6,
-                desc: '',
-                imageUrl: './images/trello-clone/img-6.png',
-            },
-            {
-                id: 7,
-                desc: '',
-                imageUrl: './images/trello-clone/img-7.png',
-            },
-            {
-                id: 8,
-                desc: '',
-                imageUrl: './images/trello-clone/img-8.png',
-            },
-            {
-                id: 9,
-                desc: '',
-                imageUrl: './images/trello-clone/img-9.png',
-            },
-            {
-                id: 10,
-                desc: '',
-                imageUrl: './images/trello-clone/img-9.png',
-            },
-            {
-                id: 11,
-                desc: '',
-                imageUrl: './images/trello-clone/img-9.png',
-            },
+        keyFeatures: ['desc-1', 'desc-2', 'desc-3', 'desc-4', 'desc-5', 'desc-6', 'desc-7', 'desc-8'],
+        snapshots: [
+            { id: 1, desc: 'desc-1', imageUrl: './images/trello-clone/img-1.png' },
+            { id: 2, desc: 'desc-2', imageUrl: './images/trello-clone/img-2.png' },
+            { id: 3, desc: 'desc-3', imageUrl: './images/trello-clone/img-3.png' },
+            { id: 4, desc: 'desc-4', imageUrl: './images/trello-clone/img-4.png' },
+            { id: 5, desc: 'desc-5', imageUrl: './images/trello-clone/img-5.png' },
+            { id: 6, desc: 'desc-6', imageUrl: './images/trello-clone/img-6.png' },
+            { id: 7, desc: 'desc-7', imageUrl: './images/trello-clone/img-7.png' },
+            { id: 8, desc: 'desc-8', imageUrl: './images/trello-clone/img-8.png' },
+            { id: 9, desc: 'desc-9', imageUrl: './images/trello-clone/img-9.png' },
+            { id: 10, desc: 'desc-10', imageUrl: './images/trello-clone/img-10.png' },
+            { id: 11, desc: 'desc-11', imageUrl: './images/trello-clone/img-11.png' },
         ],
         techStacks: ['next-js', 'typescript', 'tailwindcss', 'prisma', 'mysql'],
-        webDevToolkit: [
-            {
-                id: 'clerk',
-                logoUrl:
-                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdVEuXbieiDLdzyT-lHa1wtFVPK2ONT5utgQ&usqp=CAU',
-                name: 'Clerk',
-                desc: 'clerk-desc',
-            },
-            {
-                id: 'planetscale',
-                logoUrl:
-                    'https://images.crunchbase.com/image/upload/c_pad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/iidhf24ewhnqtjgrrfbp',
-                name: 'Planetscale',
-                desc: 'planetscale-desc',
-            },
-            {
-                id: 'shadcn',
-                logoUrl:
-                    'https://avatars.githubusercontent.com/u/139895814?s=280&v=4',
-                name: 'Shadcn',
-                desc: 'shadcn-desc',
-            },
-            {
-                id: 'stripe',
-                logoUrl:
-                    'https://b.stripecdn.com/site-statics-srv/assets/assets/img/v3/home/twitter-80afaafee00af0bc21d345164a2a9bb6.png',
-                name: 'Stripe',
-                desc: 'stripe-desc',
-            },
-            {
-                id: 'tanstack',
-                logoUrl:
-                    'https://avatars.githubusercontent.com/u/72518640?s=200&v=4',
-                name: 'Tanstack',
-                desc: 'tanstack-desc',
-            },
-            {
-                id: 'unsplash',
-                logoUrl:
-                    'https://uploads-ssl.webflow.com/5b0c471ddb589cf22d4477a4/5cd716028463a5a1d1b1e73d_unsplash-app-icon-2.png',
-                name: 'Unsplash',
-                desc: 'unsplash-desc',
-            },
-            {
-                id: 'zod',
-                logoUrl:
-                    'https://seeklogo.com/images/Z/zod-logo-B57E684330-seeklogo.com.png',
-                name: 'Zod',
-                desc: 'zod-desc',
-            },
-            {
-                id: 'zustand',
-                logoUrl: 'https://img.stackshare.io/service/11559/zustand.png',
-                name: 'Zustand',
-                desc: 'zustand-desc',
-            },
+        webDevToolkits: [
+            { id: 'clerk', name: 'Clerk', desc: 'clerk-desc' },
+            { id: 'planetscale', name: 'Planetscale', desc: 'planetscale-desc' },
+            { id: 'shadcn', name: 'Shadcn', desc: 'shadcn-desc' },
+            { id: 'stripe', name: 'Stripe', desc: 'stripe-desc' },
+            { id: 'tanstack', name: 'Tanstack', desc: 'tanstack-desc' },
+            { id: 'unsplash', name: 'Unsplash', desc: 'unsplash-desc' },
+            { id: 'zod', name: 'Zod', desc: 'zod-desc' },
+            { id: 'zustand', name: 'Zustand', desc: 'zustand-desc' },
         ],
     },
     'spotify-clone': {
         tags: ['fullStack'],
         coverImageUrl: './images/spotify-clone/img-1.png',
-        keyFeatures: [
-            'desc-1',
-            'desc-2',
-            'desc-3',
-            'desc-4',
-            'desc-5',
-            'desc-6',
-            'desc-7',
-        ],
-        snapshot: [
-            {
-                id: 1,
-                desc: '',
-                imageUrl: './images/spotify-clone/img-1.png',
-            },
-            {
-                id: 2,
-                desc: '',
-                imageUrl: './images/spotify-clone/img-2.png',
-            },
-            {
-                id: 3,
-                desc: '',
-                imageUrl: './images/spotify-clone/img-3.png',
-            },
-            {
-                id: 4,
-                desc: '',
-                imageUrl: './images/spotify-clone/img-4.png',
-            },
-            {
-                id: 5,
-                desc: '',
-                imageUrl: './images/spotify-clone/img-5.png',
-            },
-            {
-                id: 6,
-                desc: '',
-                imageUrl: './images/spotify-clone/img-6.png',
-            },
-            {
-                id: 7,
-                desc: '',
-                imageUrl: './images/spotify-clone/img-7.png',
-            },
+        keyFeatures: ['desc-1', 'desc-2', 'desc-3', 'desc-4', 'desc-5', 'desc-6', 'desc-7'],
+        snapshots: [
+            { id: 1, desc: 'desc-1', imageUrl: './images/spotify-clone/img-1.png' },
+            { id: 2, desc: 'desc-2', imageUrl: './images/spotify-clone/img-2.png' },
+            { id: 3, desc: 'desc-3', imageUrl: './images/spotify-clone/img-3.png' },
+            { id: 4, desc: 'desc-4', imageUrl: './images/spotify-clone/img-4.png' },
+            { id: 5, desc: 'desc-5', imageUrl: './images/spotify-clone/img-5.png' },
+            { id: 6, desc: 'desc-6', imageUrl: './images/spotify-clone/img-6.png' },
+            { id: 7, desc: 'desc-7', imageUrl: './images/spotify-clone/img-7.png' },
         ],
         techStacks: ['next-js', 'typescript', 'tailwindcss', 'postgresql'],
-        webDevToolkit: [
-            {
-                id: 'zustand',
-                logoUrl: 'https://img.stackshare.io/service/11559/zustand.png',
-                name: 'Zustand',
-                desc: 'zustand-desc',
-            },
-            {
-                id: 'supabase',
-                logoUrl:
-                    'https://cf.appdrag.com/dashboard-openvm-clo-b2d42c/uploads/supabase-TAiY.png',
-                name: 'Supabase',
-                desc: 'supabase-desc',
-            },
-            {
-                id: 'stripe',
-                logoUrl:
-                    'https://b.stripecdn.com/site-statics-srv/assets/assets/img/v3/home/twitter-80afaafee00af0bc21d345164a2a9bb6.png',
-                name: 'Stripe',
-                desc: 'stripe-desc',
-            },
+        webDevToolkits: [
+            { id: 'zustand', name: 'Zustand', desc: 'zustand-desc' },
+            { id: 'supabase', name: 'Supabase', desc: 'supabase-desc' },
+            { id: 'stripe', name: 'Stripe', desc: 'stripe-desc' },
         ],
     },
     'github-issues-clone': {
         tags: ['frontend'],
         coverImageUrl: './images/github-issues-clone/img-1.png',
-        snapshot: [
-            {
-                id: 1,
-                desc: '',
-                imageUrl: './images/github-issues-clone/img-1.png',
-            },
-            {
-                id: 2,
-                desc: '',
-                imageUrl: './images/github-issues-clone/img-2.png',
-            },
-            {
-                id: 3,
-                desc: '',
-                imageUrl: './images/github-issues-clone/img-3.png',
-            },
-            {
-                id: 4,
-                desc: '',
-                imageUrl: './images/github-issues-clone/img-4.png',
-            },
-            {
-                id: 5,
-                desc: '',
-                imageUrl: './images/github-issues-clone/img-5.png',
-            },
-            {
-                id: 6,
-                desc: '',
-                imageUrl: './images/github-issues-clone/img-6.png',
-            },
-            {
-                id: 7,
-                desc: '',
-                imageUrl: './images/github-issues-clone/img-7.png',
-            },
-            {
-                id: 8,
-                desc: '',
-                imageUrl: './images/spotgithub-issuesify-clone/img-7.png',
-            },
-            {
-                id: 9,
-                desc: '',
-                imageUrl: './images/github-issues-clone/img-7.png',
-            },
+        snapshots: [
+            { id: 1, desc: 'desc-1', imageUrl: './images/github-issues-clone/img-1.png' },
+            { id: 2, desc: 'desc-2', imageUrl: './images/github-issues-clone/img-2.png' },
+            { id: 3, desc: 'desc-3', imageUrl: './images/github-issues-clone/img-3.png' },
+            { id: 4, desc: 'desc-4', imageUrl: './images/github-issues-clone/img-4.png' },
+            { id: 5, desc: 'desc-5', imageUrl: './images/github-issues-clone/img-5.png' },
+            { id: 6, desc: 'desc-6', imageUrl: './images/github-issues-clone/img-6.png' },
+            { id: 7, desc: 'desc-7', imageUrl: './images/github-issues-clone/img-7.png' },
+            { id: 8, desc: 'desc-8', imageUrl: './images/spotgithub-issuesify-clone/img-8.png' },
+            { id: 9, desc: 'desc-9', imageUrl: './images/github-issues-clone/img-9.png' },
         ],
-        keyFeatures: [
-            'desc-1',
-            'desc-2',
-            'desc-3',
-            'desc-4',
-            'desc-5',
-            'desc-6',
-            'desc-7',
-            'desc-8',
-        ],
-        techStacks: [
-            'react-js',
-            'typescript',
-            'styled-components',
-            'redux',
-            'firebase',
-        ],
-        webDevToolkit: [
-            {
-                id: 'storybook',
-                logoUrl:
-                    'https://static-00.iconduck.com/assets.00/storybook-icon-412x512-1sdnathv.png',
-                name: 'StoryBook',
-                desc: 'storybook-desc',
-            },
-            {
-                id: 'supabase',
-                logoUrl:
-                    'https://cf.appdrag.com/dashboard-openvm-clo-b2d42c/uploads/supabase-TAiY.png',
-                name: 'Supabase',
-                desc: 'supabase-desc',
-            },
+        keyFeatures: ['desc-1', 'desc-2', 'desc-3', 'desc-4', 'desc-5', 'desc-6', 'desc-7', 'desc-8'],
+        techStacks: ['react-js', 'typescript', 'styled-components', 'redux', 'firebase'],
+        webDevToolkits: [
+            { id: 'storybook', name: 'StoryBook', desc: 'storybook-desc' },
+            { id: 'supabase', name: 'Supabase', desc: 'supabase-desc' },
         ],
     },
 };
