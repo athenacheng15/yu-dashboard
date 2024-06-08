3e7c4e2644f0a023a63e6e6a3c2460c5a2128b57
Athena Cheng <athenachengyc15@gmail.com>
2024-06-08 17:35:28 +0800

    fix : add stopPropagation to Card onClick event

--- app/(dashboard)/software-engineer/_components/card-footer.tsx
+++ app/(dashboard)/software-engineer/_components/card-footer.tsx
@@ -16,35 +16,33 @@ export const CardFooter = ({ repo }: { repo: DisplayRepoType }) => {
             <div className="mb-2 mr-1 text-[10px] text-main-green sm:mb-0">
                 <div className="flex items-center">
                     <PencilLine className="mr-1 h-4 w-3 text-main-green" />
-                    <p className="hidden sm:flex">
-                        Updated : {dayjs(repo.updated_at).format('YYYY/MM/DD')}
-                    </p>
-                    <p className="sm:hidden">
-                        {dayjs(repo.updated_at).format('YYYY/MM/DD')}
-                    </p>
+                    <p className="hidden sm:flex">Updated : {dayjs(repo.updated_at).format('YYYY/MM/DD')}</p>
+                    <p className="sm:hidden">{dayjs(repo.updated_at).format('YYYY/MM/DD')}</p>
                 </div>
                 <div className="flex items-center">
                     <Clock2 className="mr-1 h-4 w-3 text-main-green" />
-                    <p className="hidden sm:flex">
-                        Created : {dayjs(repo.created_at).format('YYYY/MM/DD')}
-                    </p>
-                    <p className="sm:hidden">
-                        {dayjs(repo.created_at).format('YYYY/MM/DD')}
-                    </p>
+                    <p className="hidden sm:flex">Created : {dayjs(repo.created_at).format('YYYY/MM/DD')}</p>
+                    <p className="sm:hidden">{dayjs(repo.created_at).format('YYYY/MM/DD')}</p>
                 </div>
             </div>
             <div className="flex w-full flex-col space-y-2 sm:w-auto sm:flex-row sm:justify-end sm:space-x-2 sm:space-y-0">
                 <Button
                     className="h-8"
                     variant="outline"
-                    onClick={() => window.open(repo.html_url)}
+                    onClick={e => {
+                        e.stopPropagation();
+                        window.open(repo.html_url);
+                    }}
                 >
                     <Github className="w-5" />
                 </Button>
                 <Button
                     className="h-8"
                     variant="outline"
-                    onClick={() => window.open(repo.homepage)}
+                    onClick={e => {
+                        e.stopPropagation();
+                        window.open(repo.homepage);
+                    }}
                 >
                     <Link2 className="w-5" />
                 </Button>
