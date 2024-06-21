c2a4914549d1ea2c08ea073907f1c5ac6e6b1c27
Athena Cheng <athenachengyc15@gmail.com>
2024-06-22 02:45:54 +0800

    feat : create error and not found page

--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -1,9 +1,14 @@
 'use client';
 
-// TODO : manage NotFound
+import { MapPinOff } from 'lucide-react';
 
 const NotFound = () => {
-    return <div>NotFound!!!</div>;
+    return (
+        <div className="flex h-[80vh] flex-col items-center justify-center font-semibold text-main-green">
+            <MapPinOff size={28} className="mb-4" />
+            <p className="text-lg ">Page Not Found</p>
+        </div>
+    );
 };
 
 export default NotFound;
