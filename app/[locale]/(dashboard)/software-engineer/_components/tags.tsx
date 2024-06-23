1002971c0c20876687e41967fc37a5cf0bb4e9e0
Athena Cheng <athenachengyc15@gmail.com>
2024-06-23 15:22:41 +0800

    feat : card filtered by tags

--- app/[locale]/(dashboard)/software-engineer/_components/tags.tsx
+++ app/[locale]/(dashboard)/software-engineer/_components/tags.tsx
@@ -2,28 +2,34 @@
 
 import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
 
-export const Tags = () => {
+interface ITag {
+    onChange: (value: string) => void;
+}
+
+export const Tags = ({ onChange }: ITag) => {
     const tagItem = [
         { label: 'All', value: 'all' },
-        { label: 'Next.js', value: 'nextjs' },
-        { label: 'React.js', value: 'reactjs' },
-        { label: 'Tailwind', value: 'tailwind' },
+        { label: 'Next.js', value: 'next-js' },
+        { label: 'React.js', value: 'react-js' },
+        { label: 'TypeScript', value: 'typescript' },
+        { label: 'Tailwind', value: 'tailwindcss' },
         { label: 'Prisma', value: 'prisma' },
         { label: 'MongoDB', value: 'mongodb' },
-        { label: 'MySQL', value: 'mysql' },
+        { label: 'PostgreSQL', value: 'postgresql' },
+        { label: 'Styled Components', value: 'styled-components' },
+        { label: 'Redux', value: 'redux' },
+        { label: 'Firebase', value: 'firebase' },
     ];
     return (
         <ToggleGroup
             type="single"
             size={'sm'}
+            defaultValue="all"
             className="flex w-full flex-wrap justify-start space-x-2"
+            onValueChange={onChange}
         >
             {tagItem.map(item => (
-                <ToggleGroupItem
-                    key={item.value}
-                    value={item.value}
-                    className="mb-2"
-                >
+                <ToggleGroupItem key={item.value} value={item.value} className="mb-2">
                     {item.label}
                 </ToggleGroupItem>
             ))}
--- types/repositories.ts
+++ types/repositories.ts
@@ -1,4 +1,6 @@
 import type { StaticImageData } from 'next/image';
+import type { ITechStack } from '@/database/type';
+
 export interface RepoType {
     id: number;
     name: string;
@@ -17,7 +19,7 @@ export interface DisplayRepoType {
     homepage: string;
     created_at: string;
     updated_at: string;
-    tags: string[];
+    techStacks: ITechStack[];
     coverImageUrl: StaticImageData;
     isWebsiteUnabled?: boolean;
 }
