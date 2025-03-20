81fe9fb10d6c12786de876613984690b1a6fd394
Athena Cheng <athenachengyc15@gmail.com>
2025-03-20 21:50:40 +1100

    feat : add repository database

--- database/localRepo.ts
+++ database/localRepo.ts
@@ -1,9 +1,22 @@
-export const NOTFROMGITHUBDATA = [{
-    id: crypto.randomUUID(),
-    name: "payment-gateway-client-dashboard",
-    description: "A third-party payment system's client dashboard, offering tools for secure transaction management, payment processing, and user authentication. ",
-    html_url: "",
-    homepage: "",
-    created_at:"",
-    updated_at: "",
-}]
+export const NOTFROMGITHUBDATA = [
+    {
+        id: crypto.randomUUID(),
+        name: 'payment-gateway-client-dashboard',
+        description:
+            "A third-party payment system's client dashboard, offering tools for secure transaction management, payment processing, and user authentication. ",
+        html_url: '',
+        homepage: '',
+        created_at: '',
+        updated_at: '',
+    },
+    {
+        id: crypto.randomUUID(),
+        name: 'payment-gateway-management-console',
+        description:
+            'A streamlined management console for payment gateways, offering key tools for transaction oversight, user management, and financial reporting. ',
+        html_url: '',
+        homepage: '',
+        created_at: '',
+        updated_at: '',
+    },
+];
